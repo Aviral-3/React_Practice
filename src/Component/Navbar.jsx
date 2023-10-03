8a73a705cf3229d6dc5b52e69a7ebd7b91d9f5dd
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-10-03 21:24:45 +0530

    initial

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React , {useState} from 'react';
 
 export default function Navbar(){
+    const [name, setName]= useState("name");
+
+   
     return(
-        <h1 style={{background:"green", marginTop:"0px"}}>Meal Tracker</h1>
-    )
+        <>
+        <h1 className='text-3xl font-serif italic bg-green-600'>Meal Tracker</h1>
+        <form >
+            <input className="border mt-2"type='text' value={name}onChange={(e)=>{
+                setName(e.target.value)
+               console.log(name)
+            }}/>
+            <button className='rounded-md bg-purple-500 px-2 '>Submit</button>
+            
+        </form>
+        </>
+    );
 }
