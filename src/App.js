7363c1f62ddb4065fd59e0d79ddfeba726bb50de
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-10-16 15:39:00 +0530

    5 days ago

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Component/Navbar';
 function App() {
+  function parentComp(data){
+    alert(data);
+
+  }
+  
   return (
     <div className="App">
-      <Navbar/>
+
+      <Navbar alert={parentComp}/>
      
     </div>
   );
