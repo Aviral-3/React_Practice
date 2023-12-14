954b64bbb7a5149dce02dcc18e99d7e3b4935d27
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-15 00:32:55 +0530

    practiced useState Hook

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,12 +1,32 @@
+import react,{useState} from 'react';
 export default function Counter(){
+    const [count,setCounter] = useState(0);
+    const incHandler = (count)=>{
+        
+            setCounter(count + 1);    //We don't use return in event handler functions
+        
+       }
+       const decHandler = (count)=>{
+    
+            setCounter(count - 1);
+        
+       }
     return(
         <div className="bg-orange-50">
             <div>
                 <p>Counter Check</p>
-                
+
+                <div className="flex justify-center ">
+                    <button className="rounded-xl border-black bg-purple-500 px-4 py-2 m-2 hover:scale-110  duration-200 hover:bg-purple-300" onClick={incHandler}>Increment</button>
+                    <button className="rounded-xl border-black bg-purple-500 px-4 py-2 m-2 hover:scale-110  duration-200 hover:bg-purple-300 " onClick={decHandler}>Decrement</button>
+                    <p>{count}</p>
+                </div>
+
+
+
             </div>
         
         </div>
 
-    );
+    )
 }
