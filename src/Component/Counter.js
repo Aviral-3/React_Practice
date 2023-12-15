d078514739ed4c0da1957cbb8733b4550bdf10c1
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-16 01:02:15 +0530

    useState

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,9 +1,11 @@
 import react,{useState} from 'react';
 export default function Counter(){
 const [count,setCounter] = useState(0);
+const[surprise,setSurprise] = useState('');
 const incHandler = ()=>{
-    
+        console.log("clicked increment")
         setCounter(count + 1);    //We don't use return in event handler functions
+
     
     }
     const decHandler = ()=>{
@@ -12,7 +14,9 @@ const incHandler = ()=>{
         setCounter(count - 1);
         }
     
+
     }
+ 
 return(
     <div className="bg-orange-50">
         <div>
@@ -21,8 +25,12 @@ return(
             <div className="flex justify-center ">
                 <button className="rounded-xl border-black bg-purple-500 px-4 py-2 m-2 hover:scale-110  duration-200 hover:bg-purple-300" onClick={incHandler}>Increment</button>
                 <button className="rounded-xl border-black bg-purple-500 px-4 py-2 m-2 hover:scale-110  duration-200 hover:bg-purple-300 " onClick={decHandler}>Decrement</button>
+                <p className='bg-gray-300 inline px-4 py-2 rounded-xl'>{count}</p>
             </div>
-            <p className='bg-gray-300 inline px-4 py-2 rounded-xl'>{count}</p>
+            {/* <div>
+                <button className="rounded-xl border-black bg-purple-500 px-4 py-2 m-2 hover:scale-110  duration-200 hover:bg-purple-300" onClick={handleSurprise}>Press Me!</button>
+                
+            </div> */}
 
         </div>
     
