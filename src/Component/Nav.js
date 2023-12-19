8c0215833bee41f2d75509af2a8d7235b1ac1220
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-19 22:36:17 +0530

    added softscrolling

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import {Link as Softscroll} from 'react-scroll';
 import {Link} from 'react-router-dom';
 function Nav() {
   return (
-    <div className='bg-orange-100 ' >
-        <div className='flex justify-center' >
+    <div className='bg-orange-100 p-4  ' >
+      <div className='flex justify-center'>
+        <div className='flex justify-center mx-4' >
             <ul className=''>
-                <Link className='text-slate-500 text-lg font-semibold cursor-pointer hover:underline hover:decoration-black  px-4' to="">Hero</Link>
-                <Link className='text-slate-500 text-lg font-semibold cursor-pointer hover:underline hover:decoration-black  px-4' to="/counter">Counter</Link>
+                <Softscroll className='text-slate-500 text-lg font-semibold cursor-pointer hover:underline hover:decoration-black  px-4' to="hero" smooth={true} duration={500}>Hero</Softscroll>
+                <Softscroll className='text-slate-500 text-lg font-semibold cursor-pointer hover:underline hover:decoration-black  px-4' to="counter" smooth={true} duration={500} offset={200}>Counter</Softscroll>
             </ul>
         </div>
+        <div className=''>
+          <button className='bg-gray-300 px-4 py-3 rounded-2xl hover:scale-110 transition ease-in-out duration-500'><Link to="/login">Login</Link></button>
+          
+        </div>
+      </div>
 
       
     </div>
